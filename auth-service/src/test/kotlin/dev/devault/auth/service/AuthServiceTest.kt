3c4b6f0681b431d2f8b194d803b8feab1e899746
jan <jan.zdan1234@gmail.com>
2026-06-26 17:07:33 +0200

    feat(implementing-test): made a skeleton for auth tests

---
+++ auth-service/src/test/kotlin/dev/devault/auth/service/AuthServiceTest.kt
@@ -0,0 +1,22 @@
+package dev.devault.auth.service
+
+import org.junit.jupiter.api.Test
+
+class AuthServiceTest {
+    @Test
+    fun register() {
+    }
+
+    @Test
+    fun login() {
+    }
+
+    @Test
+    fun refresh() {
+    }
+
+    @Test
+    fun logout() {
+    }
+
+}
